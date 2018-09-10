64b26e079680aa34f3d342f66004bb57dcaea65b
InevitableTobi <43132757+InevitableTobi@users.noreply.github.com>
2018-09-10 09:50:06 +0100

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,66 @@
+'''
+arr = ['abdullahi', 1,3,'hy'] #lists
+arry = []
+arry[1] = 'hello'
+print(arry[1])
+print(arr[2])
+arr
+tuples = (1,3,4)
+
+'''
+
+'''
+thisislist = [12, 14, 18, "codelagos", 1.7]
+print(thisislist)
+'''
+'''
+thisislist = [12, 14, 18, "codelagos", 1.7]
+print(thisislist[3])
+'''
+
+'''
+thisislist = [12, 14, 18, "codelagos", 1.7]
+print(len(thisislist))
+'''
+
+'''
+thisislist = [12, 14, 18, "codelagos", 1.7]
+thisislist[3] = "hello"
+print(thisislist)
+'''
+'''
+thistupple = (12,13,15,100,"hello",1.8)
+print(thistupple)
+'''
+'''
+thistupple = (12,13,15,100,"hello",1.8)
+thistupple.append(20)
+print(thistupple)
+'''
+'''
+unblocking of grey areas
+
+list
+arr = [12, 53, "codelagos", 1.7]-mutable
+
+tuples
+
+thituple = (12, 13 .......)-imutable
+
+dictionary
+Toyota_car = {color:yellow,
+                model:1221b,
+                    year:2013}
+
+person = {
+    fname:abdul,
+    sname:quda,
+    age:17
+'''
+
+Toyota_car = {"color": "yellow", "model": 12121, "year": 2014}
+print(Toyota_car["model"])
+
+Toyota_car = {"color": "yellow", "model": 12121, "year": 2014}
+del Toyota_car("year")
+print(Toyota_car)
